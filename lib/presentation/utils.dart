855acd70cb9cd19b3db294fe407231ab50b7380d
KimDaeyong <dev.kimdaeyong@gmail.com>
2019-04-30 01:05:44 +0900

    fixed conversion between second and angle.

--- lib/presentation/utils.dart
+++ lib/presentation/utils.dart
@@ -1,7 +1,9 @@
 double secondsToAngle(double seconds) {
-  return seconds * 3.6;
+  return seconds * 0.1;
 }
 
 double angleToSeconds(double angle) {
-  return angle * 10;
+  var seconds = angle * 10;
+
+  return seconds <= 0 ? 0 : seconds;
 }
